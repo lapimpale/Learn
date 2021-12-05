4dfd7cd2d1ec0ef4b1ea791bd2e506da0a176276
Pimpale <pilalit@amazon.com>
2021-12-05 22:29:07 +0530

    commit message

---
+++ Array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        String email = sc.nextLine();
+        String userName = "";
+    
+        for(int i=0; i<email.length(); i++) {
+          if(email.charAt(i) == '@') {
+           break;
+          } else {
+            userName += email.charAt(i);
+          }
+        }
+    
+        System.out.println(userName);
+      }
+   
+}
