0f9e1950e84be460586b633d5e5aa7ea4cb4bfa7
lapimpale <lalitpimpale07@gmail.com>
2021-12-13 01:06:08 +0530

    Arrays

--- Array.java
+++ Array.java
@@ -1,21 +1,31 @@
 import java.util.*;
 
 public class Array {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner (System.in);
-        String email = sc.nextLine();
-        sc.close();
-        String userName = "";
-    
-        for(int i=0; i<email.length(); i++) {
-          if(email.charAt(i) == '@') {
-           break;
-          } else {
-            userName += email.charAt(i);
-          }
-        }
-    
-        System.out.println(userName);
-      }
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int[] numbers = new int[n];
+
+    //input
+    for(int i=0; i<n; i++) {
+        numbers[i] = sc.nextInt();
+    }
+
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
    
+     for(int i=0; i<numbers.length; i++) {
+         if(numbers[i] < min) {
+             min = numbers[i];
+         }
+         if(numbers[i] > max) {
+             max = numbers[i];
+         }
+     }
+
+     System.out.println("Largest number is : " + max);
+     System.out.println("Smallest number is : " + min);
+    
+ }
 }
