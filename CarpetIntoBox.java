3c51f61398ded76ed859e23fea3e86273965edc9
lapimpale <lalitpimpale07@gmail.com>
2021-12-13 00:48:07 +0530

    update

--- CarpetIntoBox.java
+++ CarpetIntoBox.java
@@ -1,5 +1,30 @@
-public class CarpetIntoBox {
-    int carpetIntoBox (int A, int B, int C, int D){
-        int move =0;
+class CarpetIntoBox {
+    public static int carpetIntoBox(int A, int B, int C, int D){
+        int c1 = 0,c2 =0;
+        int aa = A, bb = B, cc = C, dd = D;
+
+        while(A>C||B>D){
+            if(A>C){
+            A/=2;
+        }else{
+            B/=2;
+        }
+        c1++;
+    }
+
+    while(aa>dd||bb>cc){
+        if(aa>dd){
+        aa/=2;
+    }else{
+        bb/=2;
+    }
+    c2++;
+}
+
+        return Math.min(c1,c2);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(carpetIntoBox(8,13,6,10));
     }
 }
