af1a19d53c127ea8e641986c00302ff74c769794
Lalit Pimpale <lalitpimpale07@gmail.com>
2021-12-28 13:31:25 +0530

    updated

--- MergeSort.java
+++ MergeSort.java
@@ -42,7 +42,7 @@ public class MergeSort {
 
         mergeSort(arr,0,arr.length-1);
 
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
     }
