b778d18231b2fdf7dd2f0c1ef769d48519452368
lapimpale <lalitpimpale07@gmail.com>
2022-02-17 17:44:06 +0530

    InterviewProblem

--- SubStrings.java
+++ SubStrings.java
@@ -11,7 +11,7 @@ Output: 1
 
 */
 
-
+import java.util.*;
 public class SubStrings {
     public static void main(String[] args) {
         int count = 0;
@@ -20,10 +20,27 @@ public class SubStrings {
         for(int i = 0; i< n ;i++){
             for(int j = n;j>i;j--){
                 if(s.substring(i,j).contains("a")&&s.substring(i,j).contains("b")&&s.substring(i,j).contains("c")){
+                    System.out.println(s.substring(i,j));
                     count++;
                 }
             }
         }
         System.out.println(count);
+        ArrayList<String> ans = new ArrayList<String>();
+        recursion(s, ans);
+        System.out.println(ans);
     }
+
+    static void recursion(String s, ArrayList<String> ans){
+            if(s.length()<2){
+                return;
+            }
+
+            if(s.contains("a")&&s.contains("b")&&s.contains("c")){
+                ans.add(s);
+            }
+
+            recursion(s.substring(1), ans);
+            recursion(s.substring(0,s.length()-1), ans);
+        }
 }
