fc29fabf9fe5053f57d473e33289c2bfbd9c1dec
lapimpale <lalitpimpale07@gmail.com>
2022-03-07 20:11:06 +0530

    updated

--- NumbersAppearingTwice.java
+++ NumbersAppearingTwice.java
@@ -18,7 +18,8 @@ output = [6]
 
 public class NumbersAppearingTwice {
     public static void main(String[] args) {
-        int[] data = {10, 12, 10, 11, 13, 17, 16, 15, 11};
+        int[] data = {10,19,19,1};
+        //{10, 12, 10, 11, 13, 17, 16, 15, 11};
         //int[] data = {7, 3, 8, 6, 4, 6, 7, 8, 9, 8, 7};
 
         //initializing ArrayList to store the result;
@@ -52,18 +53,22 @@ public class NumbersAppearingTwice {
     }
 
     private static void approach2(int[] data) {
-        //sorting the array
+        //sorting the array //1, 10,19,19
         Arrays.sort(data);
 
         //initializing ArrayList to store the result;
         Vector<Integer>numbersAppearingTwice = new Vector<Integer>();
 
-        int prev = data[0]; //updating 1st element of sorted array as prev number
+        int prev = data[0]; //updating 1st element of sorted array as prev number //1
         int count = 0; // initializing count as 0.
 
         for(int i =0; i<data.length;i++){
             if(data[i] == prev){ // if element is matching with previous value
                 count++; // increase the counter
+
+                if(i == data.length-1 &&count ==2){ // checking if it is the last elenment of the array and having count as 2
+                    numbersAppearingTwice.add(data[i]); // adding the same number to list.
+                }
             }else{
                 if(count == 2){ // if element is not matching with previous value and the count of appearance is 2
                     numbersAppearingTwice.add(data[i-1]); // add previous element from array to list.
