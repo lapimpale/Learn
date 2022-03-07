3c04a92ffd128cc2142b26806fc040ef9c971251
lapimpale <lalitpimpale07@gmail.com>
2022-03-07 19:52:59 +0530

    updated

--- AmazonSegments.java
+++ AmazonSegments.java
@@ -4,19 +4,21 @@ class AmazonSegments{
         int k = 3; 
         int[] weights = {1, 3, 6};
         int count = 0;
-        countSegments(weights, 0, weights.length-1, k, count);
+        countSegments(weights, weights.length-1, weights.length-1, k, count);
         System.out.println(count);
     }
 
     public static void countSegments(int arr[], int i,int j, int k, int count){
         
-        if(i==arr.length-1||j<=0){
+        if(i<=0||j<=0){
             System.out.println(arr[i]+ " " + arr[j]);
         return;
         }
 
-        countSegments(arr,i+1,j,k,count);
-        countSegments(arr,i,j-1,k,count);        
+        System.out.println(arr[i]+ " " + arr[j]);
+        for(int l =arr.length-1; l>=0;l--){
+            countSegments(arr, l, j-1, k, count);
+        }
         
     }
 }
